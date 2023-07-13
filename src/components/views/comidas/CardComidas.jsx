b5e647a25937b04220790a8fe94cf9e4e05b2b40
Mauricio Soria <mauricioutn2017@gmail.com>
2023-07-12 23:52:40 -0300

    reparado bug de responsive

--- src/components/views/comidas/CardComidas.jsx
+++ src/components/views/comidas/CardComidas.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button, Container } from 'react-bootstrap/'
+import { Card, Button, Container,Row, Col } from 'react-bootstrap/'
 import { useNavigate } from 'react-router-dom';
 const CardComidas = ({ recetas }) => {
 const navegar=useNavigate();
@@ -8,9 +8,11 @@ const navegar=useNavigate();
         <>
         <div><h1 className='text-start ms-2 mt-3'>Nuestras Selección Especial</h1></div>
         <Container className='d-flex justify-content-center my-2'>
+        <Row lg={6} md={4} sm={1}  className='d-flex justify-content-center'>
         {
+            
             recetas.map((item, index) => (
-                <Card className='mx-1' style={{ width: '18rem' }} key={index}>
+                <Card className='mx-1 mt-3' style={{ width: '18rem' }} key={index}>
                     <Card.Img variant="top" src={item.imagen} style={{height:'180px'}} />
                     <Card.Body className='d-flex flex-column'>
                         <Card.Title className='text-center'>{item.nombre}</Card.Title>
@@ -23,7 +25,9 @@ const navegar=useNavigate();
                     </Card.Body>
                 </Card>
             ))}
+        </Row>
         </Container>
+
         </>
     );
 };
