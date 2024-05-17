049f364cb76ce763f796ef06c5e1cddcc5b76964
Mauricio Soria <mauricioutn2017@gmail.com>
2024-05-17 18:01:12 -0300

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Menu from './components/common/Menu'
 import Footer from './components/common/Footer'
 import Error from './components/views/Error'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import RutasProtegidas from './components/routes/RutasProtegidas'
 import RutasAdministrador from './components/routes/RutasAdministrador'
 import Carrito from './components/views/home/Pedido/Carrito'
@@ -19,8 +19,14 @@ function App() {
   //#region hooks
   const usuarioSessionStorage = JSON.parse(sessionStorage.getItem('usuario')) || null;
   const [usuarioLogueado, setUsuarioLogueado] = useState(usuarioSessionStorage);
-  const loginUsuario=(usuario)=>setUsuarioLogueado(usuario)
-  //#endregion
+  const loginUsuario = (usuario) => setUsuarioLogueado(usuario)
+  const ip = () => {
+    const direccion=window.location.hostname;
+    console.log(direccion);
+  }
+useEffect(()=>{
+ip()
+},[])  //#endregion
   return (
     <>
       <BrowserRouter>
@@ -32,7 +38,7 @@ function App() {
           <Route exact path='/RealizarPedido' element={<RealizarPedido />} />
           <Route exact path='/Administrador/*' element={
             <RutasProtegidas>
-              <RutasAdministrador/>
+              <RutasAdministrador />
             </RutasProtegidas>
           } />
           <Route exact path="/Usuario/*" element={
@@ -46,5 +52,4 @@ function App() {
     </>
   )
 }
-
 export default App
